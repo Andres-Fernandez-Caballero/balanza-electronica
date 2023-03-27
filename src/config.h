ad5e8f1d8a77b9a652f736ec008afabe3300b5b7
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2023-03-27 11:28:04 -0300

    ADD balance feature

--- src/config.h
+++ src/config.h
@@ -1,4 +1,9 @@
 #define BAUD_SPEED 9600
 #define MESSAGE "Hello World from configs!"
 #define PRECICION_DECIMALS 2
+
+// PINs
 #define BIPPER_PIN 11
+#define HX711_SCK 12
+#define HX711_DOUT 13
+#define HX711_SCALE 145.6964285714286
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <HT1621.h>
+#include <HX711.h>
 #include "config.h"
 #include "Bipper.h"
 
@@ -7,21 +8,35 @@ HT1621 displayWeigh;
 HT1621 displayPrice; 
 HT1621 displayTotalAmount;
 
+HX711 scale;
+
 Bipper bipper(BIPPER_PIN);
 
-double weight = 1.50;
-double price = 2.00;
+long read;
+float weight = 0.00 ;
+float price = 2.00;
 
 
 void setup() {
   displayWeigh.begin(2, 3, 4);
   displayPrice.begin(5, 6, 7);
   displayTotalAmount.begin(8, 9, 10);
+
+  Serial.begin(BAUD_SPEED);
+
+  scale.begin(HX711_DOUT, HX711_SCK);
+  scale.set_scale(HX711_SCALE);
+  scale.tare();
   bipper.beep();
 }
 
 void loop() {
-  displayWeigh.print(weight, PRECICION_DECIMALS);
-  displayPrice.print(price, PRECICION_DECIMALS);
-  displayTotalAmount.print(weight * price, PRECICION_DECIMALS);
+  
+  if (scale.wait_ready_timeout(1000)) {
+    read = scale.get_units(10);
+    weight = read / 1000.00;
+    displayWeigh.print(weight, PRECICION_DECIMALS);
+    displayPrice.print(price, PRECICION_DECIMALS);
+    displayTotalAmount.print(weight * price, PRECICION_DECIMALS);
+  }
 }
